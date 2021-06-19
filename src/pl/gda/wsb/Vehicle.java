6f3861cb5f49fc4baee5861f431a431d0bce25d7
JakuSw <jakub.swastek@wp.pl>
2021-06-19 11:40:19 +0200

    Vehicle fields added

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -1,5 +1,12 @@
 package pl.gda.wsb;
 
 public abstract class Vehicle {
+    private String registrationPlate;
+    private String VINNumber;
+    private String color;
+    private int price;
+    private String combustion;
+    private int fuelInLiters;
+    private int oddometer;
 
 }

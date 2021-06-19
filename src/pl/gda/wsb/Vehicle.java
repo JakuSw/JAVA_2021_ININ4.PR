241823dec346a593f953cee1d1559a7692036515
JakuSw <jakub.swastek@wp.pl>
2021-06-19 11:52:38 +0200

    Drive and tank methods added

--- src/pl/gda/wsb/Vehicle.java
+++ src/pl/gda/wsb/Vehicle.java
@@ -1,12 +1,20 @@
 package pl.gda.wsb;
 
 public abstract class Vehicle {
+    private String name;
     private String registrationPlate;
     private String VINNumber;
     private String color;
     private int price;
     private String combustion;
     private int fuelInLiters;
-    private int oddometer;
+    private int odometer;
 
+    public void Tank(){
+        System.out.printf("%s is tanked", this.name);
+    }
+
+    public void Drive(){
+        System.out.printf("%s is in move", this.name);
+    }
 }
